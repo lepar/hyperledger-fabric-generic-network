8ec93b867250737131c46fb6d78ed094728d4520
Nicholas <nlzanutim@yahoo.com>
2019-11-09 14:05:48 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # hyperledger-fabric-generic-network
+
+A simple and generic hyperledger fabric network that runs with Raft consensus and TLS enabled.
+It deploys a network with:
+    - 1 Certificate Authority
+    - 3 orderers for the raft consensus to work nicely
+    - 1 Peer
+    - 1 CouchDB
+    - 1 CLI
+
+
+Pre Requisites:
+Refer to https://hyperledger-fabric.readthedocs.io/en/release-1.4/prereqs.html 
+Requires the latest versions of node and npm to be installed
+
+This network works only on Fabric 1.4.3 and it is up to production standards using Raft and TLS for secure communication
+
+To deploy a network simply run deploy.sh with 3 parameters:
+
+    - Organization Name
+    - Organization Domain 
+    - Host computers IP Address
+
+Example:
+./deploy.sh Org1 org1.com 192.168.0.0
+
+To get the host computers ip address, you can run "hostname -I"
+
+I am not responsible for the misuse of this generic code nor any damages that may occurr from improper use or development. 
+This code is open source and free for anyone to use for any type of project or application.
+Feel free to email me with questions or suggestions
+
+Contact:
+nlzanutim@yahoo.com
+
+Donations are welcome as I intend on continuing to contribute to the community
+Bitcoin: bc1qndlhlznpcqp63hacj4w28ph6vxjzua942ftqse
+Litecoin: ltc1q2vzn2ztts5lf8am4h5paukjv8xha5ajte8rpg2
+Monero: 45yWqAH7ynhGUXtwx2nuNn14avPXNcdTwfa1aWbU5tbm1oBtiVCLo4fSD83nG6K5JeC1kwtLRbWqsadtCuodXeYnStbMtGw
+Dogecoin: DR6tUDs8YKbfqiRSwtN5fXATr9YNHGbXQY
+Ethereum: 0x4F6e88c170F438EC2529f6bbA921c0236b3b45c4
---
+++ deploy.sh
@@ -0,0 +1,112 @@
+#set -ev
+#!/bin/sh
+
+# Organization information that you wish to build and deploy
+export NAME_OF_ORGANIZATION=$1
+export DOMAIN_OF_ORGANIZATION=$2
+export HOST_COMPUTER_IP_ADDRESS=$3
+export ORGANIZATION_NAME_LOWERCASE=`echo "$NAME_OF_ORGANIZATION" | tr '[:upper:]' '[:lower:]'`
+export CA_ADDRESS_PORT=ca.$DOMAIN_OF_ORGANIZATION:7054
+
+# Security defaults
+# Couch DB credentials
+export COUCH_DB_USERNAME=admin
+export COUCH_DB_PASSWORD=adminpw
+
+# Certificate authority credentials
+export CA_ADMIN_USER=admin
+export CA_ADMIN_PASSWORD=adminpw
+
+# Orderer credentials
+ORDERER_PASSWORD=adminpw
+
+# Peer credentials
+PEER_PASSWORD=peerpw
+
+./clean-all.sh
+
+# Substitutes organizations information in the configtx template to match organizations name, domain and ip address
+sed -e 's/organization_name/'$NAME_OF_ORGANIZATION'/g' -e 's/organization_domain/'$DOMAIN_OF_ORGANIZATION'/g' -e 's/ip_address/'$HOST_COMPUTER_IP_ADDRESS'/g'  configtx_template.yaml > configtx.yaml
+
+# Start the certficate authority
+docker-compose -p fabric-network -f docker-compose.yml up -d ca
+sleep 3
+
+# Generate identity and cryptographic materials for the 3 orderers 
+for ORDERER_NUMBER in 1 2 3
+do
+  docker exec ca.$DOMAIN_OF_ORGANIZATION /bin/bash -c "cd /etc/hyperledger/artifacts/  && ./orderer-identity.sh $CA_ADDRESS_PORT $DOMAIN_OF_ORGANIZATION $HOST_COMPUTER_IP_ADDRESS $CA_ADMIN_USER $CA_ADMIN_PASSWORD $ORDERER_NUMBER $ORDERER_PASSWORD"
+done
+
+# Generate identity and cryptographic materials for the peer 
+docker exec ca.$DOMAIN_OF_ORGANIZATION /bin/bash -c "cd /etc/hyperledger/artifacts/  && ./peer-identity.sh $CA_ADDRESS_PORT $DOMAIN_OF_ORGANIZATION $HOST_COMPUTER_IP_ADDRESS $PEER_PASSWORD"
+
+# Move the crypto-config folder to manipulate it more easily away from the dockers users' restrictions
+sudo mv ./${ORGANIZATION_NAME_LOWERCASE}Ca/client/crypto-config ./
+sudo chmod -R 777 ./crypto-config
+
+# Move TLS certificates for the 3 orderers 
+for ORDERER_NUMBER in 1 2 3
+do
+  ORDERER_DIRECTORY=./crypto-config/ordererOrganizations/orderers
+  sudo mv $ORDERER_DIRECTORY/orderer$ORDERER_NUMBER.$DOMAIN_OF_ORGANIZATION/tls/signcerts/cert.pem $ORDERER_DIRECTORY/orderer$ORDERER_NUMBER.$DOMAIN_OF_ORGANIZATION/tls/server.crt
+  sudo mv $ORDERER_DIRECTORY/orderer$ORDERER_NUMBER.$DOMAIN_OF_ORGANIZATION/tls/keystore/*_sk $ORDERER_DIRECTORY/orderer$ORDERER_NUMBER.$DOMAIN_OF_ORGANIZATION/tls/server.key
+  sudo mv $ORDERER_DIRECTORY/orderer$ORDERER_NUMBER.$DOMAIN_OF_ORGANIZATION/tls/tlscacerts/*.pem $ORDERER_DIRECTORY/orderer$ORDERER_NUMBER.$DOMAIN_OF_ORGANIZATION/tls/ca.crt
+  
+  # Delete empty directories
+  sudo rm -rf $ORDERER_DIRECTORY/orderer$ORDERER_NUMBER.$DOMAIN_OF_ORGANIZATION/tls/{cacerts,keystore,signcerts,tlscacerts,user}
+done
+
+# Peers crypto-config directory
+PEER_DIRECTORY=./crypto-config/peerOrganizations/peers/peer.$DOMAIN_OF_ORGANIZATION
+
+# Move the Peer TLS files to match cryptogen hierarchy
+sudo mv $PEER_DIRECTORY/tls/signcerts/cert.pem $PEER_DIRECTORY/tls/server.crt
+sudo mv $PEER_DIRECTORY/tls/keystore/*_sk $PEER_DIRECTORY/tls/server.key
+sudo mv $PEER_DIRECTORY/tls/tlscacerts/*.pem $PEER_DIRECTORY/tls/ca.crt
+
+# Delete the peers empty directory
+sudo rm -rf $PEER_DIRECTORY/tls/{cacerts,keystore,signcerts,tlscacerts,user}
+
+# Generate the channel configuration 
+./generate.sh ${ORGANIZATION_NAME_LOWERCASE}channel $NAME_OF_ORGANIZATION
+sleep 2
+
+# Start the network with docker-compose
+docker-compose -f docker-compose.yml up -d peer couchdb cli 
+sleep 2
+docker-compose -f docker-compose.yml up -d orderer
+docker-compose -f docker-compose.yml up -d orderer2 
+docker-compose -f docker-compose.yml up -d orderer3
+sleep 15
+
+# Creates the channel
+docker exec cli peer channel create -o orderer1.$DOMAIN_OF_ORGANIZATION:7050 -c ${ORGANIZATION_NAME_LOWERCASE}channel --tls --cafile /etc/hyperledger/crypto-config/ordererOrganizations/orderers/orderer1.$DOMAIN_OF_ORGANIZATION/tls/ca.crt -f /etc/hyperledger/artifacts/channel.tx
+
+# Joins the peer to the channel
+docker exec cli peer channel join -b ${ORGANIZATION_NAME_LOWERCASE}channel.block
+
+# Build javescript chaincode
+pushd chaincode
+npm install
+npm run build
+popd 
+
+# Install the chaincode
+docker exec cli peer chaincode install -n chaincode -v 1.0 -p /etc/hyperledger/chaincode -l node
+
+# Instantiate the chaincode
+docker exec cli peer chaincode instantiate -o orderer1.$DOMAIN_OF_ORGANIZATION:7050 -C ${ORGANIZATION_NAME_LOWERCASE}channel -n chaincode -v 1.0 -l node -c '{"Args":["initLedger"]}' -P "OR('${NAME_OF_ORGANIZATION}MSP.member')" --tls --cafile /etc/hyperledger/crypto-config/ordererOrganizations/orderers/orderer1.$DOMAIN_OF_ORGANIZATION/tls/ca.crt
+
+sleep 5
+
+# Test the chaincode
+# Invoke a transaction
+docker exec cli peer chaincode invoke -o orderer1.$DOMAIN_OF_ORGANIZATION:7050 -C ${ORGANIZATION_NAME_LOWERCASE}channel -n chaincode -c '{"Args":["invokeTransaction","1","{anythingHereAsJsonPayload}"]}' --tls --cafile /etc/hyperledger/crypto-config/ordererOrganizations/orderers/orderer1.$DOMAIN_OF_ORGANIZATION/tls/ca.crt
+
+sleep 3
+
+# Query the blockchain
+docker exec cli peer chaincode query -C ${ORGANIZATION_NAME_LOWERCASE}channel -n chaincode -c '{"Args":["queryBlockchain","1"]}' --tls --cafile /etc/hyperledger/crypto-config/ordererOrganizations/orderers/orderer1.$DOMAIN_OF_ORGANIZATION/tls/ca.crt
+
+# NETWORK DEPLOYMENT COMPLETED SUCCESSFULLY
