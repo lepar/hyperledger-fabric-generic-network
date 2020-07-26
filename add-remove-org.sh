24fc2f92e0b5c2e757c3d9fce9ca517b4da08698
Zanutim <nlzanutim@yahoo.com>
2020-07-26 12:27:44 -0300

    Fixed script to add or remove new orgnaization

--- add-remove-org.sh
+++ add-remove-org.sh
@@ -1,5 +1,3 @@
-# SPDX-License-Identifier: Apache-2.0
-
 # Input organization info parameters
 OPERATION=$1
 ORG_NAME=$2
@@ -17,11 +15,18 @@ function ordererEnvVariables(){
     export CORE_PEER_TLS_ROOTCERT_FILE=/etc/hyperledger/crypto-config/ordererOrganizations/orderers/orderer1.$ORGANIZATION_DOMAIN/tls/ca.crt
 }
 
-function peerEnvVariables(){
+function peerEnvVariablesForBlock(){
     export CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/crypto-config/ordererOrganizations/users/Admin@orderer1.$ORGANIZATION_DOMAIN/msp
     export CORE_PEER_ADDRESS=peer.$ORGANIZATION_DOMAIN:7051
     export CORE_PEER_LOCALMSPID=${ORG_ADMIN}OrdererMSP
     export CORE_PEER_TLS_ROOTCERT_FILE=/etc/hyperledger/crypto-config/ordererOrganizations/orderers/orderer1.$ORGANIZATION_DOMAIN/tls/ca.crt
+}
+
+function peerEnvVariables(){
+    export CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/crypto-config/peerOrganizations/users/Admin@peer.$ORGANIZATION_DOMAIN/msp
+    export CORE_PEER_ADDRESS=peer.$ORGANIZATION_DOMAIN:7051
+    export CORE_PEER_LOCALMSPID=${ORG_ADMIN}MSP
+    export CORE_PEER_TLS_ROOTCERT_FILE=/etc/hyperledger/crypto-config/peerOrganizations/orderers/peer.$ORGANIZATION_DOMAIN/tls/ca.crt
 
 }
 
@@ -30,15 +35,21 @@ function fetchConfigBlock(){
     configtxlator proto_decode --input config_block.pb --type common.Block | jq .data.data[0].payload.data.config > config.json
 }
 
-function addOrgSystemChannel(){
+function addOrgChannel(){
     # Add to organization channel group
     jq -s '.[0] * {"channel_group":{"groups":{"Orderer":{"groups": {"'$ORG_NAME'OrdererMSP":.[1]}}}}}' config.json ${ORG_NAME}OrdererMSP.json > modified_config.json
 
     # Add orderer address to addresses list
     jq ".channel_group.values.OrdererAddresses.value.addresses += [\"$ORGANIZATION_IP_ADDRESS:7050\"]" modified_config.json > modified_config1.json
 
-    # Add new Org MSP to Consortium channel group
-    jq -s ".[0] * {\"channel_group\":{\"groups\":{\"Consortiums\":{\"groups\":{\"${ORG_ADMIN}Consortium\":{\"groups\": {\"${ORG_NAME}MSP\":.[1]}}}}}}}" modified_config1.json ${ORG_NAME}MSP.json > modified_config2.json
+    if [ $CHANNEL_NAME == "system-channel" ]; then
+        # Add new Org MSP to Consortium channel group
+        jq -s ".[0] * {\"channel_group\":{\"groups\":{\"Consortiums\":{\"groups\":{\"${ORG_ADMIN}Consortium\":{\"groups\": {\"${ORG_NAME}MSP\":.[1]}}}}}}}" modified_config1.json ${ORG_NAME}MSP.json > modified_config2.json
+
+    else
+        echo "ADDING TO APPLI CHANNEL"
+        jq -s ".[0] * {\"channel_group\":{\"groups\":{\"Application\":{\"groups\":{\"${ORG_NAME}MSP\":.[1]}}}}}" modified_config1.json ${ORG_NAME}MSP.json > modified_config2.json
+    fi
 
     # Add to consenters list
     export FLAG=$(if [ "$(uname -s)" == "Linux" ]; then echo "-w 0"; else echo "-b 0"; fi)
@@ -47,6 +58,7 @@ function addOrgSystemChannel(){
     echo "{\"client_tls_cert\":\"$(cat $TLS_FILE | base64 $FLAG)\",\"host\":\"$ORGANIZATION_IP_ADDRESS\",\"port\":7050,\"server_tls_cert\":\"$(cat $TLS_FILE | base64 $FLAG)\"}" > $PWD/new-consenter.json
 
     jq ".channel_group.groups.Orderer.values.ConsensusType.value.metadata.consenters += [$(cat new-consenter.json)]" modified_config2.json > modified_config_final.json
+
 }
 
 function removeOrg(){
@@ -78,29 +90,41 @@ function sendUpdate(){
 
     peer channel signconfigtx -f org_update_in_envelope.pb
 
+    if [ $CHANNEL_NAME != "system-channel" ]; then
+        peerEnvVariables
+    fi
+
     peer channel update -f org_update_in_envelope.pb -c $CHANNEL_NAME -o orderer1.$ORGANIZATION_DOMAIN:7050 --tls --cafile /etc/hyperledger/crypto-config/ordererOrganizations/orderers/orderer1.$ORGANIZATION_DOMAIN/tls/ca.crt
+
 }
 
 if [ $OPERATION == "add" ]; then
 
     mkdir $ORG_DIRECTORY/org-temp
+
+    cp ${ORG_NAME}OrdererMSP.json ./org-temp
+    cp ${ORG_NAME}MSP.json ./org-temp
+    cp server.crt ./org-temp
+
     pushd $ORG_DIRECTORY/org-temp
 
     # Add to system channel
     export CHANNEL_NAME='system-channel'
     ordererEnvVariables
     fetchConfigBlock
-    addOrgSystemChannel
+    addOrgChannel
     sendUpdate
 
     sleep 5
 
     # Add to application channel
     export CHANNEL_NAME=$CHANNEL
-    peerEnvVariables
+    peerEnvVariablesForBlock
     fetchConfigBlock
-    addOrgSystemChannel
+    addOrgChannel
     sendUpdate
+
+    popd
 
 elif [ $OPERATION == "remove" ]; then
 
@@ -118,10 +142,11 @@ elif [ $OPERATION == "remove" ]; then
 
     # Remove from application channel
     export CHANNEL_NAME=$CHANNEL
-    peerEnvVariables
+    peerEnvVariablesForBlock
     fetchConfigBlock
     removeOrg
     sendUpdate
 
     popd
 fi
+
